2abca4bf9500e93099ac8f43875e81f5b0e12ff8
Sanchith Velmurugan <150633347+SanchithVel@users.noreply.github.com>
2025-07-29 00:26:24 -0500

    Remove Javadoc in Encryption.java

--- SecureNoteApp/src/Encryption.java
+++ SecureNoteApp/src/Encryption.java
@@ -2,39 +2,17 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Arrays;
 
-/**
- * 
- * This class handles AES encryption and decryption.
- * @author Sanchith Velmurugan
- * @version 1.1
- * 
- */
 public class Encryption {
     private static final String ALGO = "AES";
     private static final int KEY_SIZE = 16;
-
-    /**
-     * This method encrypts text using AES with a key derived from the password.
-     * @param String data
-     * @param String password
-     * @return the encrypted byte array
-     * @throws Exception if encryption fails
-     * 
-     */
+	
     public static byte[] encrypt(String data, String password) throws Exception {
         SecretKeySpec key = getKey(password);
         Cipher cipher = Cipher.getInstance(ALGO);
         cipher.init(Cipher.ENCRYPT_MODE, key);
         return cipher.doFinal(data.getBytes("UTF-8"));
     }
-
-    /**
-     * This method decrypts encrypted data using the original password.
-     * @param byte[] encryptedData
-     * @param String password
-     * @return String original decrypted text
-	 * @throws Exception if encryption fail
-     */
+	
     public static String decrypt(byte[] encryptedData, String password) throws Exception {
         SecretKeySpec key = getKey(password);
         Cipher cipher = Cipher.getInstance(ALGO);
@@ -43,12 +21,6 @@ public class Encryption {
         return new String(decrypted, "UTF-8");
     }
 
-    /**
-     * This method converts a password into a AES key.
-     * @param String password
-     * @return SecretKeySpec key based on the given password
-     * @throws Exception if key generation fail
-     */
     private static SecretKeySpec getKey(String password) throws Exception {
         byte[] keyBytes = Arrays.copyOf(password.getBytes("UTF-8"), KEY_SIZE);
         return new SecretKeySpec(keyBytes, ALGO);
